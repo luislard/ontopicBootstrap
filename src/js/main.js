a86765ba9e28f6f5ea4d6e8c01994fc8d94273d9
Luis Rosales <eng.luisrosales@gmail.com>
2017-08-06 17:16:24 +0200

    adding the service to the main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,5 @@
 window.jQuery = require("jquery"); // Hace jQuery accesible públicamente
 
+import ArticleService from "../ArticleService";
 
+const articleService = new ArticleService("/article/");
