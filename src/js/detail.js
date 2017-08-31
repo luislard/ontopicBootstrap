8743926d2a2f3322b7074b996a17768b8b628ee5
Luis Rosales <eng.luisrosales@gmail.com>
2017-09-01 00:19:17 +0200

    trying to add the comments

--- src/js/detail.js
+++ src/js/detail.js
@@ -1,11 +1,13 @@
 window.jQuery = require("jquery"); // Hace jQuery accesible públicamente
 import ArticleService from "../ArticleService";
+import CommentService from "../CommentService";
 import ArticleDetailManager from "../ArticleDetailManager";
 import PubSub from "pubsub-js";
 import FooterManager from "../FooterManager";
 
 
 const articleService = new ArticleService("/articles/");
+const commentService = new CommentService("/comments/");
 
 const articleDetailManager = new ArticleDetailManager(".article-detail", articleService, PubSub);
 articleDetailManager.init();
